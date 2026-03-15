97ba7f55cd86603aba71ae4fb580f2155a2dfe24
maxpang93 <maxpang93@gmail.com>
2026-03-15 11:22:48 +0800

    Init project + working S3 bucket navigation if bucket CORS configured

---
+++ src/StorageBrowser.tsx
@@ -0,0 +1,42 @@
+import { createStorageBrowser } from "@aws-amplify/ui-react-storage/browser";
+import "@aws-amplify/ui-react-storage/styles.css";
+
+const region = import.meta.env.VITE_AWS_REGION
+const accountId = import.meta.env.VITE_ACCOUNT_ID
+const accessKeyId = import.meta.env.VITE_AWS_ACCESS_KEY_ID
+const secretAccessKey = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+const bucket = import.meta.env.VITE_S3_BUCKET
+
+export const { StorageBrowser } = createStorageBrowser({
+  config: {
+    region: region,
+    accountId: accountId,
+
+    listLocations: async () => {
+      return {
+        items: [
+          {
+            id: "bucket1",
+            bucket: bucket,
+            prefix: "",
+            type: "BUCKET",
+            permissions: ["list", "get", "write", "delete"]
+          }
+        ]
+      };
+    },
+
+    getLocationCredentials: async () => {
+      return {
+        credentials: {
+          accessKeyId: accessKeyId,
+          secretAccessKey: secretAccessKey,
+          sessionToken: undefined,
+          expiration: new Date(Date.now() + 3600 * 1000)
+        }
+      };
+    },
+
+    registerAuthListener: () => {}
+  }
+});
